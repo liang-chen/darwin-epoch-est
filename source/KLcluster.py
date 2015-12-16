29f2e9ca3eb1e81d47f6b1c571bd3a146302595c
Liang Chen <chen348@indiana.edu>
2015-12-16 10:52:06 -0500

    use multiple clusters to generate richer surprise patterns

--- source/KLcluster.py
+++ source/KLcluster.py
@@ -3,9 +3,25 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 from myClass import mGaussian
+from math import log
 import random
 
-#simple experiment
+def get_kl_dist(list_p,list_q):
+    return sum([p*log(p/q,2) for p,q in zip(list_p, list_q)])
+
+def get_mean_cols(matrix):
+    s = []
+    for i in xrange(len(matrix)):
+        if len(s) == 0:
+            s = matrix[i]
+        else:
+            s = [x+y for x,y in zip(s, matrix[i])]
+    if len(s):
+        s = [x/len(matrix) for x in s]
+    #print 'haha'
+    #print(sum(s))
+    return s
+
 def KL_pair_trivial(KL_matrix):
     pList = []
     for i in xrange(len(KL_matrix)):
@@ -17,18 +33,28 @@ def KL_pair_trivial(KL_matrix):
     pairs = np.array(pList)
     return pairs
 
-def KL_pair(KL_matrix):
+def KL_pair(KL_matrix, Topics_matrix):
     pList = []
+    global_kl = []
+    
+    if(len(Topics_matrix) == 0):
+        return np.array([])
+    
+    past = [1.0/ len(Topics_matrix[0,])]*len(Topics_matrix[0,])
+    for i in xrange(len(Topics_matrix)):
+        cur = Topics_matrix[i,]
+        global_kl.append(get_kl_dist(cur, past))
+        past = get_mean_cols(Topics_matrix[0:i+1])
+    
     for i in xrange(len(KL_matrix)):
         row = KL_matrix[i,:]
-        
         if(i == 0):
             local_kl = 0
-            global_kl = 0
+        #global_kl = 0
         else:
             local_kl = row[i-1]
-            global_kl = sum(row[:i])/i
-        pList.append([local_kl, global_kl])
+        #global_kl = sum(row[:i])/i
+        pList.append([local_kl, global_kl[i]])
     pairs = np.array(pList)
     return pairs
 
@@ -62,17 +88,15 @@ def learn_KL_gaussian(k_means, pairs):
         my_center = sum(pairs[my_members,:]) / len(pairs[my_members,:])#k_means_cluster_centers[k]
         my_data = pairs[my_members,:]
         my_cov = np.cov(my_data, rowvar = 0)
-        print my_cov
+        #print my_cov
         kGaussians.append(mGaussian(my_center, my_cov))
     return kGaussians
 
-def learn_from_KL(KL_matrix, n):
-    pairs = KL_pair(KL_matrix)
-    #pairs_t = KL_pair_trivial(KL_matrix)
-
+def learn_from_KL(KL_matrix, Topics_matrix, n):
+    pairs = KL_pair(KL_matrix, Topics_matrix)
     k_means_list = []
 
-    for i in xrange(1, n+1):
+    for i in xrange(2, n+1):
         k_means_list.append(KMC(pairs, i))
 
     #k_means_t = KMC(pairs_t, n)
--- source/darwin_data_explore.py
+++ source/darwin_data_explore.py
@@ -4,14 +4,16 @@
 import sys
 import csv
 import numpy as np
+from math import log
 from KLcluster import learn_from_KL
 from myClass import mGaussian
 from epoch_decode import estimate_epochs
 
 def main():
-    num_topics = sys.argv[1]
+    num_topics = int(sys.argv[1])
     num_epochs = int(sys.argv[2])
     filepath = "../data/KLs/kl_dists{0}.csv".format(num_topics)
+    filepath_topics = "../data/topics/topics{0}.csv".format(num_topics)
     names = []
     dat_rows = []
     with open(filepath, 'r') as csvfile:
@@ -20,10 +22,19 @@ def main():
         for row in reader:
             names.append(row[0])
             dat_rows.append([float(x) for x in row[1:]])
-        
-    data = np.array(dat_rows)
-    
-    pairs, kGaussians = learn_from_KL(data,3)
+    data_kl = np.array(dat_rows)
+
+    names_new = []
+    dat_rows_new = []
+    with open(filepath_topics, 'r') as csvfile:
+        reader = csv.reader(csvfile, delimiter = ',')
+        next(reader, None)
+        for row in reader:
+            names_new.append(row[0])
+            dat_rows_new.append([float(x) for x in row[1:]])
+
+    data_topics = np.array(dat_rows_new)
+    pairs, kGaussians = learn_from_KL(data_kl,data_topics,4)
     estimate_epochs(pairs, kGaussians, num_epochs)   
 
 if __name__ == "__main__":
